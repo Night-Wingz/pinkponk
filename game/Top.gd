32b3ee947ea6d7bacc9726e83ba1af9445e141f6
Night-Wingz <125206660+Night-Wingz@users.noreply.github.com>
2023-02-19 15:45:57 +0300

    pushladık

---
+++ game/Top.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+var speed = Vector2(200,0)
+
+func _process(delta):
+	position += speed.rotated(rotation) * delta
+
+
+func _on_Area2D_body_entered(body):
+	rotation = -rotation
+	speed = -speed
+	rotation_degrees += rand_range(90,-90)
